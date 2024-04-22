fb37d93b8e84488d5e367d081c9438806338faf6
Sergey <benifeks72@gmail.com>
2024-04-22 22:49:28 +0300

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BlackjackComponent } from './components/blackjack/blackjack.component';
+import { HorsesComponent } from './components/horses/horses.component';
+import { TableComponent } from './components/table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blackjack', component: BlackjackComponent },
+  { path: 'horses', component: HorsesComponent },
+  { path: 'table', component: TableComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlackjackComponent } from './components/blackjack/blackjack.component';
+import { HorsesComponent } from './components/horses/horses.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HomeComponent,
+    BlackjackComponent,
+    HorsesComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
